ae3c5fb4f9501c206970490ced115939bbaf4c5e
M.J <janelidze.marita@kiu.edu.ge>
2023-09-08 14:15:20 +0400

    Initial Commit - Solution

--- src/main/java/com/epam/rd/autocode/RowMapperFactory.java
+++ src/main/java/com/epam/rd/autocode/RowMapperFactory.java
@@ -1,10 +1,44 @@
 package com.epam.rd.autocode;
 
 import com.epam.rd.autocode.domain.Employee;
+import com.epam.rd.autocode.domain.FullName;
+import com.epam.rd.autocode.domain.Position;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.ResultSet;
+import java.time.LocalDate;
 
 public class RowMapperFactory {
 
     public RowMapper<Employee> employeeRowMapper() {
-        throw new UnsupportedOperationException();
+
+        class RowMapperImpl implements RowMapper<Employee> {
+
+            @Override
+            public Employee mapRow(ResultSet resultSet) {
+                try {
+                    BigInteger id = resultSet.getBigDecimal("ID").toBigInteger();
+                    String firstName = resultSet.getString("FIRSTNAME");
+                    String lastName = resultSet.getString("LASTNAME");
+                    String middleName = resultSet.getString("MIDDLENAME");
+                    Position position = Position.valueOf(resultSet.getString("POSITION"));
+                    LocalDate hired = resultSet.getDate("HIREDATE").toLocalDate();
+                    BigDecimal salary = resultSet.getBigDecimal("SALARY");
+
+                    return new Employee(
+                            id,
+                            new FullName(firstName, lastName, middleName),
+                            position,
+                            hired,
+                            salary);
+                }
+                catch (Exception e) {
+                    throw new UnsupportedOperationException();
+                }
+            }
+        }
+
+        return new RowMapperImpl();
     }
 }
